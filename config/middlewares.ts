a1fca2a2a57df9f94355e02b122637105c2b2917
mirza36 <mirzamohd1214@gmail.com>
2026-07-17 18:10:20 +0400

    gemini-strapi

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -3,7 +3,44 @@ import type { Core } from '@strapi/strapi';
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': [
+            "'self'",
+            'https:',
+            'https://darsemarefat.blob.core.windows.net',
+          ],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://darsemarefat.blob.core.windows.net',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://darsemarefat.blob.core.windows.net',
+          ],
+          'frame-src': [
+            "'self'",
+            'http://localhost:3000',
+          ],
+          'frame-ancestors': [
+            "'self'",
+            'http://localhost:3000',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,18 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  upload: {
+    config: {
+      provider: 'strapi-provider-upload-azure-storage',
+      providerOptions: {
+        authType: 'default',
+        account: env('AZURE_STORAGE_ACCOUNT'),
+        accountKey: env('AZURE_STORAGE_ACCOUNT_KEY'),
+        containerName: env('AZURE_STORAGE_CONTAINER_NAME'),
+        defaultPath: 'media',
+      },
+    },
+  },
+});
 
-export default config;
+export default config;  
